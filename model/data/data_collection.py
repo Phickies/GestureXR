47259068916c94c8e791ec47d037623529cba10e
Phicks <tranquyan@student.utwente.nl>
2024-04-19 01:26:51 +0200

    Fixing data

---
+++ model/data/data_collection.py
@@ -0,0 +1,83 @@
+import serial
+import csv
+from datetime import datetime
+import keyboard
+
+# Open serial port for receiving data from Arduino
+# ser = serial.Serial('COM6', 921600, timeout=1)
+ser = serial.Serial('COM7', 921600, timeout=1)
+
+# Create a CSV file
+csv_file_path = 'python1.csv'
+
+# Open file for data logging+
+file = open(csv_file_path, "w", newline='')
+csv_writer = csv.writer(file)
+
+try:
+    # Write CSV header with Timestamp, sensor data, label and sep columns
+    csv_header = ["Timestamp", "data",
+                  "Label", "Sep"]
+    csv_writer.writerow(csv_header)
+
+    # Initialize label and previous sign
+    labels = ["pinch2finger", "release2finger", "pinch3finger", "release3finger", "fists"]  # List of possible labels
+    label_index = 0  # Index for cycling through labels
+    current_label = labels[label_index]
+    sep = False  # Initial sep value
+
+
+    def clean_and_join_numbers(data_str):
+        cleaned_numbers = ''.join(data_str.replace('-', ''))
+        return cleaned_numbers
+
+    while True:
+        # Read data from Arduino
+        # for _ in range(3):
+        line = ser.readline().decode().strip()
+        data = line.strip("'").split("\t\t")
+        data_row = [clean_and_join_numbers(data_str) for data_str in data]
+        # print(len(data))
+
+        # if len(data) == 1:
+        #     data_row.extend(data)
+        print(data_row)
+
+        if len(data_row) == 1:  # Ensure we have exactly 6 sensor values
+            # Get current timestamp1
+            current_timestamp = datetime.now().strftime('%H:%M:%S.%f')
+
+            # Check keyboard input to set sep and current_label
+            if keyboard.is_pressed('1'):
+                current_label = labels[0]
+                sep = True
+            elif keyboard.is_pressed('2'):
+                current_label = labels[1]
+                sep = True
+            elif keyboard.is_pressed('3'):
+                current_label = labels[2]
+                sep = True
+            elif keyboard.is_pressed('4'):
+                current_label = labels[3]
+                sep = True
+            elif keyboard.is_pressed('5'):
+                current_label = labels[4]
+                sep = True
+            else:
+                sep = False
+
+            # Write data to CSV file along with label and sep
+            # print(data_row)
+            csv_writer.writerow([current_timestamp] + data_row + [current_label, sep])
+
+
+except serial.SerialException as e:
+    print(f"Serial communication error: {e}")
+
+finally:
+    # Close serial port
+    ser.close()
+
+    # Close file and save data to CSV
+    file.close()
+    print("Data saved to", csv_file_path)
---
+++ model/data/data_preprocess.py
@@ -0,0 +1,122 @@
+import ast
+import os
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# Declare variable
+data_unprocessed_path = 'data/data_collected_unprocessed/'
+
+# Config panda
+pd.set_option('display.max_columns', None)
+
+
+def convert_string_to_list(string):
+    """
+    Convert string into list
+    :param string:
+    :return: list of integer
+    """
+    int_list = [int(item) for item in string.strip("[]").strip("'").split("', ' ")]
+    return int_list
+
+
+def removed_error_data(string):
+    a = "Error collecting data on middle finger"
+    b = "Error collecting data on thumb finger"
+    c = "Error collecting data on index finger"
+    if a in string:
+        return string.strip(a)
+    return string
+
+
+def merge_csv_file(folder_path):
+    """
+    Merge all data collected into one big data frame
+    :param folder_path: input folder
+    :return: panda DataFrame
+    """
+    # List to store individual DataFrame
+    df_list = []
+
+    # Iterate over each file in the folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.csv'):
+            # Read the CSV file into a DataFrame
+            file_path = os.path.join(folder_path, filename)
+            dataframe = pd.read_csv(file_path)
+
+            # Append the df to the list
+            df_list.append(dataframe)
+
+    # Concatenate all DataFrame in the list into one big DataFrame
+    return pd.concat(df_list, ignore_index=True)
+
+
+def convert_str_to_int(dataframe):
+    """
+    Convert the value in the Accel and Gyr from list of string to list of int
+    :param dataframe: df need to change
+    :return: panda DataFrame
+    """
+    # Convert string values in lists to integers
+    dataframe.Accel = dataframe.Accel.apply(lambda x: convert_string_to_list(x))
+    dataframe.Gyr = dataframe.Gyr.apply(lambda x: convert_string_to_list(x))
+
+    return dataframe
+
+
+def fix_sep_value(dataframe):
+    """
+    Fix the duplicate True value, switching back to False.
+    :param dataframe: df need to change
+    :return: pd DataFrame
+    """
+    # Find the indices where 'Sep' is True
+    true_indices = dataframe.index[dataframe.Sep == True]
+
+    # Iterate over the indices and update the value of 'Sep' to False for adjacent True values
+    for idx in range(len(true_indices) - 1):
+        if true_indices[idx + 1] - true_indices[idx] == 1:
+            dataframe.at[true_indices[idx + 1], 'Sep'] = False
+    return dataframe
+
+
+def fix_add_sep_for_pinch2finger(dataframe):
+    """
+    Fix the dataframe by adding a True sep for each pinch2finger
+    :param dataframe: df need to change
+    :return: pd DataFrame
+    """
+    # Find the indices where the label is 'pinch2finger'
+    pinch2finger_indices = dataframe.index[dataframe['Label'] == 'pinch2finger']
+
+    # Iterate over the indices and update the value of 'Sep' to True only for the first occurrence
+    for idx in pinch2finger_indices:
+        # Check if the index is greater than 0 and the previous row's label is not 'pinch2finger'
+        if idx > 0 and dataframe.at[idx - 1, 'Label'] != 'pinch2finger':
+            dataframe.at[idx, 'Sep'] = True
+
+    return dataframe
+
+
+def get_data():
+    print("Merge dataset")
+    df = merge_csv_file(data_unprocessed_path)
+    df.to_csv("testing_data",index=False)
+    print("Removed NaN and duplicated")
+    df = df.dropna()
+    df = df.drop_duplicates()
+    print("Convert str to int")
+    df = convert_str_to_int(df)
+    print("Fix sep value")
+    df = fix_sep_value(df)
+    df = fix_add_sep_for_pinch2finger(df)
+    print("Done preprocessing data")
+
+    return df
+
+
+if __name__ == "__main__":
+    pass

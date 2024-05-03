dbb2afda6514c1b149c2a160e31e18216f2b046c
Phicks <tranquyan@student.utwente.nl>
2024-05-03 07:00:01 +0200

    Adding Doc String.

--- source/source.ino
+++ source/source.ino
@@ -27,7 +27,8 @@
 /* 
   Class finger to abstract IMU and data in each finger
   Attribute:
-    Vector3 accleration, velocity, position, rotateSpeed;
+    Quaternion q0, q1, q2, q3;
+    Vector3 accleration, rotateSpeed;
     BMI270 imu;
     String name;
   Method:
@@ -35,101 +36,12 @@
     void setupConfig(bmi2_sens_config accelConfig, bmi2_sens_config gyroConfig, bmi2_int_pin_config intConfig);
     void calibrateSensor();
     void getRawData();
-    void filterData();
-    void integrateMotion();
+    void filterDataAHSR();
     void serialPrintRawData();
-    void serialPrintPosition();
+    void serialPrintQuaternion();
   */
 class Finger {
-private:
-  float deg2rad(float value) {
-    return (float)(PI / 180) * value;
-  }
-
-  float integrate(float value, float dt) {
-    return value * dt;
-  }
-
-  void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az, const float sampleFreq, const float beta) {
-    float recipNorm;
-    float s0, s1, s2, s3;
-    float qDot1, qDot2, qDot3, qDot4;
-    float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2, _8q1, _8q2;
-    float q0q0, q1q1, q2q2, q3q3;
-
-    // Rate of change of quaternion from gyroscope
-    qDot1 = 0.5f * (-this->q1 * gx - this->q2 * gy - this->q3 * gz);
-    qDot2 = 0.5f * (this->q0 * gx + this->q2 * gz - this->q3 * gy);
-    qDot3 = 0.5f * (this->q0 * gy - this->q1 * gz + this->q3 * gx);
-    qDot4 = 0.5f * (this->q0 * gz + this->q1 * gy - this->q2 * gx);
-
-    // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
-    if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
-      // Normalize accelerometer measurement
-      recipNorm = invSqrt(ax * ax + ay * ay + az * az);
-      ax *= recipNorm;
-      ay *= recipNorm;
-      az *= recipNorm;
-
-      // Auxiliary variables to avoid repeated arithmetic
-      _2q0 = 2.0f * this->q0;
-      _2q1 = 2.0f * this->q1;
-      _2q2 = 2.0f * this->q2;
-      _2q3 = 2.0f * this->q3;
-      _4q0 = 4.0f * this->q0;
-      _4q1 = 4.0f * this->q1;
-      _4q2 = 4.0f * this->q2;
-      _8q1 = 8.0f * this->q1;
-      _8q2 = 8.0f * this->q2;
-      q0q0 = this->q0 * this->q0;
-      q1q1 = this->q1 * this->q1;
-      q2q2 = this->q2 * this->q2;
-      q3q3 = this->q3 * this->q3;
-
-      // Gradient decent algorithm corrective step
-      s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-      s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * this->q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
-      s2 = 4.0f * q0q0 * this->q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
-      s3 = 4.0f * q1q1 * this->q3 - _2q1 * ax + 4.0f * q2q2 * this->q3 - _2q2 * ay;
-      recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3);  // normalize step magnitude
-      s0 *= recipNorm;
-      s1 *= recipNorm;
-      s2 *= recipNorm;
-      s3 *= recipNorm;
-
-      // Apply feedback step
-      qDot1 -= beta * s0;
-      qDot2 -= beta * s1;
-      qDot3 -= beta * s2;
-      qDot4 -= beta * s3;
-    }
-
-    // Integrate rate of change of quaternion to yield quaternion
-    this->q0 += qDot1 * (1.0f / sampleFreq);
-    this->q1 += qDot2 * (1.0f / sampleFreq);
-    this->q2 += qDot3 * (1.0f / sampleFreq);
-    this->q3 += qDot4 * (1.0f / sampleFreq);
-
-    // Normalize quaternion
-    recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-    this->q0 *= recipNorm;
-    this->q1 *= recipNorm;
-    this->q2 *= recipNorm;
-    this->q3 *= recipNorm;
-  }
-
-  // Fast inverse square-root
-  float invSqrt(float x) {
-    float halfx = 0.5f * x;
-    float y = x;
-    long i = *(long*)&y;
-    i = 0x5f3759df - (i >> 1);
-    y = *(float*)&i;
-    y = y * (1.5f - (halfx * y * y));
-    return y;
-  }
-
-public:
+  public:
 
   volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;
 
@@ -223,6 +135,7 @@ public:
     this->rotateSpeed.z = this->imu.data.gyroZ;
   }
 
+  // Filter the data with fancy MadgwickAHSR Algorithm
   void fileDataAHSR(const float sampleFreq, const float beta) {
     this->MadgwickAHRSupdateIMU(
       deg2rad(this->rotateSpeed.x),
@@ -271,6 +184,96 @@ public:
     Serial.print(", ");
     Serial.println(this->q3);
   }
+
+private:
+  float deg2rad(float value) {
+    return (float)(PI / 180) * value;
+  }
+
+  float integrate(float value, float dt) {
+    return value * dt;
+  }
+
+  void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az, const float sampleFreq, const float beta) {
+    float recipNorm;
+    float s0, s1, s2, s3;
+    float qDot1, qDot2, qDot3, qDot4;
+    float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2, _8q1, _8q2;
+    float q0q0, q1q1, q2q2, q3q3;
+
+    // Rate of change of quaternion from gyroscope
+    qDot1 = 0.5f * (-this->q1 * gx - this->q2 * gy - this->q3 * gz);
+    qDot2 = 0.5f * (this->q0 * gx + this->q2 * gz - this->q3 * gy);
+    qDot3 = 0.5f * (this->q0 * gy - this->q1 * gz + this->q3 * gx);
+    qDot4 = 0.5f * (this->q0 * gz + this->q1 * gy - this->q2 * gx);
+
+    // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+    if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+      // Normalize accelerometer measurement
+      recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+      ax *= recipNorm;
+      ay *= recipNorm;
+      az *= recipNorm;
+
+      // Auxiliary variables to avoid repeated arithmetic
+      _2q0 = 2.0f * this->q0;
+      _2q1 = 2.0f * this->q1;
+      _2q2 = 2.0f * this->q2;
+      _2q3 = 2.0f * this->q3;
+      _4q0 = 4.0f * this->q0;
+      _4q1 = 4.0f * this->q1;
+      _4q2 = 4.0f * this->q2;
+      _8q1 = 8.0f * this->q1;
+      _8q2 = 8.0f * this->q2;
+      q0q0 = this->q0 * this->q0;
+      q1q1 = this->q1 * this->q1;
+      q2q2 = this->q2 * this->q2;
+      q3q3 = this->q3 * this->q3;
+
+      // Gradient decent algorithm corrective step
+      s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+      s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * this->q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+      s2 = 4.0f * q0q0 * this->q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+      s3 = 4.0f * q1q1 * this->q3 - _2q1 * ax + 4.0f * q2q2 * this->q3 - _2q2 * ay;
+      recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3);  // normalize step magnitude
+      s0 *= recipNorm;
+      s1 *= recipNorm;
+      s2 *= recipNorm;
+      s3 *= recipNorm;
+
+      // Apply feedback step
+      qDot1 -= beta * s0;
+      qDot2 -= beta * s1;
+      qDot3 -= beta * s2;
+      qDot4 -= beta * s3;
+    }
+
+    // Integrate rate of change of quaternion to yield quaternion
+    this->q0 += qDot1 * (1.0f / sampleFreq);
+    this->q1 += qDot2 * (1.0f / sampleFreq);
+    this->q2 += qDot3 * (1.0f / sampleFreq);
+    this->q3 += qDot4 * (1.0f / sampleFreq);
+
+    // Normalize quaternion
+    recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+    this->q0 *= recipNorm;
+    this->q1 *= recipNorm;
+    this->q2 *= recipNorm;
+    this->q3 *= recipNorm;
+  }
+
+  // Fast inverse square-root
+  float invSqrt(float x) {
+    float halfx = 0.5f * x;
+    float y = x;
+    long i = *(long*)&y;
+    i = 0x5f3759df - (i >> 1);
+    y = *(float*)&i;
+    y = y * (1.5f - (halfx * y * y));
+    return y;
+  }
+
+
 };
 
 //-----------------------------------------

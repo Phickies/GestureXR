20aaab63bbe49848da3178e6a2504ce1a3ff2e93
Phicks <tranquyan@student.utwente.nl>
2024-05-03 06:47:55 +0200

    Finish, add Quaternion MadgwickAHRS filter

--- source/source.ino
+++ source/source.ino
@@ -9,8 +9,10 @@
 #define SCL_2_PIN 14
 #define TOUCH_PIN 1
 
+#define PI 3.14159
+
 #define COM_RATE 400000  // 400KHz
-#define DT 0.1           // 100 ms
+#define DT 0.001         // 1 ms
 
 #define ACC_RANGE BMI2_ACC_RANGE_16G  // 16G
 #define ACC_ODR BMI2_ACC_ODR_1600HZ   // 1600Hz
@@ -33,42 +35,114 @@
     void setupConfig(bmi2_sens_config accelConfig, bmi2_sens_config gyroConfig, bmi2_int_pin_config intConfig);
     void calibrateSensor();
     void getRawData();
+    void filterData();
+    void integrateMotion();
     void serialPrintRawData();
+    void serialPrintPosition();
   */
 class Finger {
-public:
-  struct Vector3 {
-    float x, y, z;
-  };
+private:
+  float deg2rad(float value) {
+    return (float)(PI / 180) * value;
+  }
 
-  String name;
-  Vector3 acceleration, velocity, position, rotateSpeed;
-  BMI270 imu;
-
-  Finger(const String& name)
-    : name(name), acceleration({ 0, 0, 0 }), velocity({ 0, 0, 0 }), position({ 0, 0, 0 }), rotateSpeed({ 0, 0, 0 }){};
-
-  // Intergarte value
   float integrate(float value, float dt) {
     return value * dt;
   }
 
-  // Set velocity through integration acceleration
-  void setVelocity(float dt) {
-    this->velocity.x = this->integrate(this->acceleration.x, dt);
-    this->velocity.y = this->integrate(this->acceleration.y, dt);
-    this->velocity.z = this->integrate(this->acceleration.z, dt);
+  void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az, const float sampleFreq, const float beta) {
+    float recipNorm;
+    float s0, s1, s2, s3;
+    float qDot1, qDot2, qDot3, qDot4;
+    float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2, _8q1, _8q2;
+    float q0q0, q1q1, q2q2, q3q3;
+
+    // Rate of change of quaternion from gyroscope
+    qDot1 = 0.5f * (-this->q1 * gx - this->q2 * gy - this->q3 * gz);
+    qDot2 = 0.5f * (this->q0 * gx + this->q2 * gz - this->q3 * gy);
+    qDot3 = 0.5f * (this->q0 * gy - this->q1 * gz + this->q3 * gx);
+    qDot4 = 0.5f * (this->q0 * gz + this->q1 * gy - this->q2 * gx);
+
+    // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+    if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+      // Normalize accelerometer measurement
+      recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+      ax *= recipNorm;
+      ay *= recipNorm;
+      az *= recipNorm;
+
+      // Auxiliary variables to avoid repeated arithmetic
+      _2q0 = 2.0f * this->q0;
+      _2q1 = 2.0f * this->q1;
+      _2q2 = 2.0f * this->q2;
+      _2q3 = 2.0f * this->q3;
+      _4q0 = 4.0f * this->q0;
+      _4q1 = 4.0f * this->q1;
+      _4q2 = 4.0f * this->q2;
+      _8q1 = 8.0f * this->q1;
+      _8q2 = 8.0f * this->q2;
+      q0q0 = this->q0 * this->q0;
+      q1q1 = this->q1 * this->q1;
+      q2q2 = this->q2 * this->q2;
+      q3q3 = this->q3 * this->q3;
+
+      // Gradient decent algorithm corrective step
+      s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+      s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * this->q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+      s2 = 4.0f * q0q0 * this->q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+      s3 = 4.0f * q1q1 * this->q3 - _2q1 * ax + 4.0f * q2q2 * this->q3 - _2q2 * ay;
+      recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3);  // normalize step magnitude
+      s0 *= recipNorm;
+      s1 *= recipNorm;
+      s2 *= recipNorm;
+      s3 *= recipNorm;
+
+      // Apply feedback step
+      qDot1 -= beta * s0;
+      qDot2 -= beta * s1;
+      qDot3 -= beta * s2;
+      qDot4 -= beta * s3;
+    }
+
+    // Integrate rate of change of quaternion to yield quaternion
+    this->q0 += qDot1 * (1.0f / sampleFreq);
+    this->q1 += qDot2 * (1.0f / sampleFreq);
+    this->q2 += qDot3 * (1.0f / sampleFreq);
+    this->q3 += qDot4 * (1.0f / sampleFreq);
+
+    // Normalize quaternion
+    recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+    this->q0 *= recipNorm;
+    this->q1 *= recipNorm;
+    this->q2 *= recipNorm;
+    this->q3 *= recipNorm;
   }
 
-  void setPosition(float dt) {
-    this->position.x = this->integrate(this->velocity.x, dt);
-    this->position.y = this->integrate(this->velocity.y, dt);
-    this->position.z = this->integrate(this->velocity.z, dt);
+  // Fast inverse square-root
+  float invSqrt(float x) {
+    float halfx = 0.5f * x;
+    float y = x;
+    long i = *(long*)&y;
+    i = 0x5f3759df - (i >> 1);
+    y = *(float*)&i;
+    y = y * (1.5f - (halfx * y * y));
+    return y;
   }
 
-  void convertToPosition(){
+public:
 
-  }
+  volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f;
+
+  struct Vector3 {
+    float x, y, z;
+  };
+
+  Vector3 acceleration, rotateSpeed;
+  String name;
+  BMI270 imu;
+
+  Finger(const String& name)
+    : name(name), acceleration({ 0, 0, 0 }), rotateSpeed({ 0, 0, 0 }){};
 
   // Setup I2C port and establish connection with the IMU sensor
   void setupI2C(TwoWire* i2cBus, uint8_t sensorAddress) {
@@ -104,7 +178,7 @@ public:
     this->imu.setInterruptPinConfig(intConfig);
   }
 
-  /* 
+  /*
     Calibration data
 
     Perform component retrim for the gyroscope. According to the datasheet,
@@ -149,6 +223,18 @@ public:
     this->rotateSpeed.z = this->imu.data.gyroZ;
   }
 
+  void fileDataAHSR(const float sampleFreq, const float beta) {
+    this->MadgwickAHRSupdateIMU(
+      deg2rad(this->rotateSpeed.x),
+      deg2rad(this->rotateSpeed.y),
+      deg2rad(this->rotateSpeed.z),
+      this->acceleration.x,
+      this->acceleration.y,
+      this->acceleration.z,
+      sampleFreq,
+      beta);
+  }
+
   // Serial print raw acceleration and gyroscope
   void serialPrintRawData() {
     Serial.print(this->acceleration.x);
@@ -161,15 +247,29 @@ public:
     Serial.print(", ");
     Serial.print(this->rotateSpeed.y);
     Serial.print(", ");
-    Serial.println(this->rotateSpeed.z);
+    Serial.print(this->rotateSpeed.z);
+    Serial.println(", ");
   }
 
-  void serialPrintPosition() {
-    Serial.print(this->position.x);
+  void serialPrintQuaternion() {
+    Serial.print(this->q0);
     Serial.print(", ");
-    Serial.print(this->position.y);
+    Serial.print(this->q1);
     Serial.print(", ");
-    Serial.println(this->position.z);
+    Serial.print(this->q2);
+    Serial.print(", ");
+    Serial.print(this->q3);
+    Serial.print(", ");
+  }
+
+  void serialPrintQuaternionEnd() {
+    Serial.print(this->q0);
+    Serial.print(", ");
+    Serial.print(this->q1);
+    Serial.print(", ");
+    Serial.print(this->q2);
+    Serial.print(", ");
+    Serial.println(this->q3);
   }
 };
 
@@ -234,8 +334,14 @@ void loop() {
 
   if (touchVal) {
     middleFinger.getRawData();
-    middleFinger.serialPrintRawData();
-    delay(DT * 1000);
+    indexFinger.getRawData();
+    thumbFinger.getRawData();
+    middleFinger.fileDataAHSR(400, 0.1);
+    indexFinger.fileDataAHSR(400, 0.1);
+    thumbFinger.fileDataAHSR(400, 0.1);
+    middleFinger.serialPrintQuaternion();
+    indexFinger.serialPrintQuaternion();
+    thumbFinger.serialPrintQuaternionEnd();
   }
 }
 
